06f5b3dbdff7c60a1225ccf66d89d14c54340144
the-halfbloodprince <ammanishkumardas@gmail.com>
2020-11-10 10:29:26 +0530

    idkw

--- screept.js
+++ screept.js
@@ -1,5 +1,3 @@
-window.addEventListener(onscroll,()=>{
-    var CQlogo=document.getElementById('main');
-    //The logo should move from the center of the screen to the top left while shrinking in size at the same time
-    
+ScrollOut({
+    targets:'h,p,div';
 })
